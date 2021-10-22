69c595713f08bbb50f90900ee66288570546b9d9
Andrew Creekmore <andrew.creekmore@me.com>
2021-10-22 14:52:25 -0500

    config update

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,10 +1,10 @@
 // ./database/db-connector.js
 
 // get an instance of mysql we can use in the app
-const pool = require("mysql");
+var mysql = require("mysql");
 
 // create a connection using credentials
-var connection = mysql.createPool({
+var pool = mysql.createPool({
     host: "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "nt3gj8186ds60d3d",
     password: "co67xuxa7wukbkv3",
