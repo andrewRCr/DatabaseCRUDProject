36d84b1b300fdbb1942063632f076f0b1332435c
Andrew Creekmore <andrew.creekmore@me.com>
2021-10-23 13:34:00 -0500

    added form and post route

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // app.js
 
-// SETUP AND ROUTING
+// SETUP
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', exphbs({                     // Create an instance of the handlebars engine to process templates
     extname: ".hbs"
 }));
+var db = require('./database/db-connector')
+
+// use public folder; set templating engine
 app.use(express.static('public'));
 app.set('view engine', '.hbs');
-var db = require('./database/db-connector')
+
+// handle JSON and form data
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 // use Heroku-defined port
 const port = process.env.PORT || 3000;
 
-
-
+// ROUTING
 // define default route
 app.get('/', (req, res) =>
     {  
@@ -27,6 +33,57 @@ app.get('/', (req, res) =>
     });                                                         // received back from the query
 
 
+app.post('/add-person-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let homeworld = parseInt(data.homeworld);
+    if (isNaN(homeworld))
+    {
+        homeworld = 'NULL'
+    }
+
+    let age = parseInt(data.age);
+    if (isNaN(age))
+    {
+        age = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES ('${data.fname}', '${data.lname}', ${homeworld}, ${age})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM bsg_people;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
 
 
  // LISTENER
