e8f39eb3c5b013ea4b1f5345472db72315483a21
wassersg <71936408+wassersg@users.noreply.github.com>
2021-10-23 19:20:26 -0500

    Update cs340_projectgroup10.sql

--- database/cs340_projectgroup10.sql
+++ database/cs340_projectgroup10.sql
@@ -15,3 +15,28 @@ CREATE TABLE IF NOT EXISTS `Yards` (
 
 ALTER TABLE Employees ADD COLUMN assigned_yard INT(11), ADD FOREIGN KEY (assigned_yard) references Yards(yard_id);
 
+CREATE TABLE IF NOT EXISTS `Dogs` (
+  dog_id int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+  dog_name varchar(255) NOT NULL,
+  size int(1) NOT NULL,
+  assigned_yard int(11) NOT NULL,
+  assigned_kennel int(11) NOT NULL,
+  FOREIGN KEY (`assigned_yard`) REFERENCES `Yards` (`id`),
+  FOREIGN KEY (`assigned_kennel`) REFERENCES `Kennels` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `Kennels` (
+  kennel_id int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+  size_limit int(1) NOT NULL,
+  current_tennant int(11),
+  FOREIGN KEY (`current_tennant`) REFERENCES `Dogs` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `Dogs_Employees_Relations` (
+  `dog_id` int(11),
+  `emp_id` int(11),
+  `get_along` int(1),
+  PRIMARY KEY (`dog_id`, `emp_id`),
+  FOREIGN KEY (`dog_id`) REFERENCES `Dogs` (`dog_id`),
+  FOREIGN KEY (`emp_id`) REFERENCES `Employees` (`emp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
