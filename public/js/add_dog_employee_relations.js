9e7dd29ba4dfa1d7be97fd0de3d5f6a00f0b82ef
wassersg <71936408+wassersg@users.noreply.github.com>
2021-10-23 20:45:45 -0500

    Create add_dog_employee_relations.js

---
+++ public/js/add_dog_employee_relations.js
@@ -0,0 +1,89 @@
+// ./public/js/add_dog_employee_relation.js
+
+// Get the objects we need to modify
+let addDogEmployeeRelationForm = document.getElementById('add-dog_employee_relations-form-ajax');
+
+// Modify the objects we need
+addEmployeeForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputDogid = document.getElementById("input-dog_id");
+    let inputEmpid = document.getElementById("input-emp_id");
+    let inputGetAlong = document.getElementById("input-get_along");
+
+    // Get the values from the form fields
+    let dogidValue = inputDogid.value;
+    let empidValue = inputEmpid.value;
+    let getAlongValue = inputGetAlong.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        dog_id: dogidValue,
+        emp_id: empidValue,
+        get_along: getAlongValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-dog_employee_relations-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputDogid.value = '';
+            inputEmpid.value = '';
+            inputGetAlong.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Dog_Employee_Relations
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("dog_employee_relations-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let dogidCell = document.createElement("TD");
+    let empidCell = document.createElement("TD");
+    let getAlongCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    dogidCell.innerText = newRow.dog_id;
+    empidCell.innerText = newRow.emp_id;
+    getAlongCell.innerText = newRow.get_along;
+
+    // Add the cells to the row 
+    row.appendChild(dogidCell);
+    row.appendChild(empCell);
+    row.appendChild(getAlongCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
