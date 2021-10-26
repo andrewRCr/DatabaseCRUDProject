bc233b4c8a5db973f07d35488251a3a8178f0dd6
wassersg <71936408+wassersg@users.noreply.github.com>
2021-10-26 11:16:41 -0500

    Update cs340_projectgroup10.sql

--- database/cs340_projectgroup10.sql
+++ database/cs340_projectgroup10.sql
@@ -13,29 +13,26 @@ CREATE TABLE IF NOT EXISTS `Yards` (
   dog_limit int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- reference to yard_id can't be added as FK to Employees until Yards table is created
 ALTER TABLE Employees ADD COLUMN assigned_yard INT(11), ADD FOREIGN KEY (assigned_yard) references Yards(yard_id);
 
 CREATE TABLE IF NOT EXISTS `Dogs` (
   dog_id int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
   dog_name varchar(255) NOT NULL,
-  size int(1) NOT NULL,
-  assigned_yard int(11) NOT NULL,
-  assigned_kennel int(11) NOT NULL,
-  FOREIGN KEY (`assigned_yard`) REFERENCES `Yards` (`yard_id`)
+  size int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE Dogs ADD COLUMN assigned_yard INT(11), ADD FOREIGN KEY (assigned_yard) references Yards(yard_id);
 
 CREATE TABLE IF NOT EXISTS `Kennels` (
   kennel_id int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-  size_limit int(1) NOT NULL,
-  current_tennant int(11),
-  FOREIGN KEY (`current_tennant`) REFERENCES `Dogs` (`dog_id`)
+  size_limit int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- reference to kennel_id can't be added to Dogs as FK until Kennels table is created
-ALTER TABLE Dogs ADD FOREIGN KEY (assigned_kennel) references Kennels(kennel_id);
 
-CREATE TABLE IF NOT EXISTS `Dog_Employee_Relations` (
+ALTER TABLE Dogs ADD COLUMN assigned_kennel INT(11), ADD FOREIGN KEY (assigned_kennel) references Kennels(kennel_id);
+ALTER TABLE Kennels ADD COLUMN current_tenant INT(11), ADD FOREIGN KEY (current_tenant) references Dogs(dog_id);
+
+CREATE TABLE IF NOT EXISTS `Dogs_Employees_Relations` (
   `dog_id` int(11),
   `emp_id` int(11),
   `get_along` int(1),
