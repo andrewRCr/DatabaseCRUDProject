e34830aa2c438dd7f378486f19e5081f6769379f
wassersg <71936408+wassersg@users.noreply.github.com>
2021-10-26 12:36:17 -0500

    Update cs340_projectgroup10.sql

--- database/cs340_projectgroup10.sql
+++ database/cs340_projectgroup10.sql
@@ -18,7 +18,7 @@ ALTER TABLE Employees ADD COLUMN assigned_yard INT(11), ADD FOREIGN KEY (assigne
 CREATE TABLE IF NOT EXISTS `Dogs` (
   dog_id int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
   dog_name varchar(255) NOT NULL,
-  size int(1) NOT NULL
+  dog_size int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE Dogs ADD COLUMN assigned_yard INT(11), ADD FOREIGN KEY (assigned_yard) references Yards(yard_id);
