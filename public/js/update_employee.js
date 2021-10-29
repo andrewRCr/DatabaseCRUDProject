183acfb226ba0e70a8aa418af2693fb11000de0f
wassersg <71936408+wassersg@users.noreply.github.com>
2021-10-28 20:46:06 -0500

    Update update_employee.js
    Added a redirect upon successful update

--- public/js/update_employee.js
+++ public/js/update_employee.js
@@ -51,6 +51,7 @@ updateEmployeeForm.addEventListener("submit", function (e) {
             inputPhoneNumber.value = '';
             inputJobTitle.value = '';
             inputAssignedYard.value = '';
+            window.location.href = "/employees";
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
             console.log("There was an error with the input.")
